9cde7e5c80bad958cbfdcc017033c1c4f070f15d
James Keicher <jamesdkeicher@gmail.com>
2020-10-30 21:35:07 -0700

    Latest Version

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ choices = [1,2,3,4,5,6,7,8,9]
 class Player:
     def __init__(self):
         pass
-        
 
     def player1(self):
         player1 = input("Enter a number between 1 and 9 : ")
@@ -22,8 +21,8 @@ class Player:
         return player2
 
 class Game(Player):
-    def __init__(self):
-        # super().__init__ (self)
+    def __init__(self, game_on):
+        self.game_on = game_on
         player1 = self.player1()
         play = int(player1)
         for i in board:                    
@@ -32,9 +31,8 @@ class Game(Player):
                 if int(play) == i[j]:       
                     i[j] = "x"            
                     choices.remove(int(play))
-                    # print(game)
-        
-                
+        self.Win_loss()
+                                   
         player2 = self.player2()
         for i in board:                    
             for j in i:                     
@@ -42,6 +40,7 @@ class Game(Player):
                 if player2 == i[j]:
                     i[j] = "o" 
                     choices.remove(player2)
+        self.Win_loss()
         game = f""" 
             {board[0][0]} | {board[0][1]} | {board[0][2]}
             --+---+--
@@ -50,8 +49,62 @@ class Game(Player):
             {board[2][0]} | {board[2][1]} | {board[2][2]}
         """
         print(game)
+    def Win_loss(self):
+        for i in board:
+            if i == ["x","x","x"]:
+                self.game_on = ''
+                print("You Win")
+            if board[0][0] == board[1][0] == board[2][0] == "x":
+                self.game_on = ''
+                print("you Win")
+            elif board[0][1] == board[1][1] == board[2][1] == "x":
+                self.game_on = ''
+                print("you Win")
+            elif board[0][2] == board[1][2] == board[2][2] == "x":
+                self.game_on = ''
+                print("you Win")
+            elif board[0][0] == board[1][1] == board[2][2] == "x":
+                self.game_on = ''
+                print("you Win")
+            elif board[0][2] == board[1][1] == board[2][0] == "x":
+                self.game_on = ''
+                print("you Win")
+                return self.game_on
 
-Game()
+        for i in board:
+            if i == ["o","o","o"]:
+                self.game_on = ''
+                print("You Lose")
+            if board[0][0] == board[1][0] == board[2][0] == "o":
+                self.game_on = ''
+                print("you Lose")
+            elif board[0][1] == board[1][1] == board[2][1] == "o":
+                self.game_on = ''
+                print("you Lose")
+            elif board[0][2] == board[1][2] == board[2][2] == "o":
+                self.game_on = ''
+                print("you Lose")
+            elif board[0][0] == board[1][1] == board[2][2] == "o":
+                self.game_on = ''
+                print("you Lose")
+            elif board[0][2] == board[1][1] == board[2][0] == "o":
+                self.game_on = ''
+                print("you Lose")
+            return self.game_on
+
+
+class Tic():
+    def __init__(self):
+        game_on = "x"
+        while game_on:
+            Game(game_on)
+            Games = Game(game_on)
+            self.game_on = Games.Win_loss()
+            print(game_on)
+        
+
+
+Tic()
 
 
 
